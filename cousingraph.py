63e1b6997a5e4886e451d0d32d139046fe2042db
PerezIngrid <35306786+PerezIngrid@users.noreply.github.com>
2018-01-22 11:40:05 -0600

    Add files via upload

---
+++ cousingraph.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+plt.plot([6,6,8,9,10,10,10,11,14,14,15,16,17,17,17,17,17,17,17,17,18,18]) 
+plt.ylabel('Hermanos y primos por cada anio de mi vida [UA]')
+plt.xlabel('Edad [anios]')
+plt.title('Ingrid Perez Francisco')
+plt.savefig('temp.png')
+plt.show() 
